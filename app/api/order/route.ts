8a8342d9d93da0283297d8a1b7c3d870b1ca80b8
PRASHANTSWAROOP001 <mrprashantswaroop@gmail.com>
2025-07-12 23:08:38 +0530

    added a check to prevent double subscription payment or stacking at order creation

--- app/api/order/route.ts
+++ app/api/order/route.ts
@@ -1,9 +1,7 @@
-
-import { NextRequest, NextResponse } from "next/server"
-import Razorpay from "razorpay"
-import z, {string, ZodError} from "zod"
-import {prisma} from "../../../lib/prisma"
-import crypto from "crypto"
+import { NextRequest, NextResponse } from "next/server";
+import Razorpay from "razorpay";
+import z from "zod";
+import { prisma } from "../../../lib/prisma";
 
 
 type RazorpayOrderResponse = {
@@ -21,92 +19,107 @@ type RazorpayOrderResponse = {
   created_at: number;
 };
 
-
-
-const Subscription = z.enum([
-    "MONTHLY",
-    "QUARTERLY",
-    "HALF_YEARLY",
-    "YEARLY"
-])
+const Subscription = z.enum(["MONTHLY", "QUARTERLY", "HALF_YEARLY", "YEARLY"]);
 
 const orderSchema = z.object({
-    pricingId:z.string(),
-    clerkId:z.string()
-})
+  pricingId: z.string(),
+  clerkId: z.string(),
+});
 
-
-type orderType = z.infer<typeof orderSchema>
+type orderType = z.infer<typeof orderSchema>;
 
 if (!process.env.RAZOR_KEY_ID || !process.env.RAZOR_SECRET_ID) {
   throw new Error("Razorpay environment variables are missing");
 }
 
-
 const razorPay = new Razorpay({
-    key_id:process.env.RAZOR_KEY_ID as string,
-    key_secret:process.env.RAZOR_SECRET_ID as string,
-})
+  key_id: process.env.RAZOR_KEY_ID as string,
+  key_secret: process.env.RAZOR_SECRET_ID as string,
+});
 
+export async function POST(req: NextRequest) {
+  try {
+    const body = await req.json();
 
+    const parsedData = orderSchema.safeParse(body);
 
-export async function POST(req:NextRequest){
-    try {
-        
-        const body = await req.json();
-
-        const parsedData = orderSchema.safeParse(body);
-
-        if(!parsedData.success){
-            return NextResponse.json({
-                success:false,
-                message:`validation failed: ${parsedData.error.message}`
-            }, {status:400})
-        }
-
-        const fetchPricingPlan = await prisma.pricing.findUnique({
-            where:{
-                id:parsedData.data.pricingId,
-            }
-        })
-
-        if(!fetchPricingPlan){
-            return NextResponse.json({
-                success:false,
-                message:"could not find the plan"
-            }, {status:404})
-        }
-
-        const createNewOrder = await razorPay.orders.create({
-            amount:fetchPricingPlan.price,
-            currency:fetchPricingPlan.currency,
-            receipt:parsedData.data.clerkId,
-            notes: { Subscription: fetchPricingPlan.subscription, clerkId:parsedData.data.clerkId }
-        }) as RazorpayOrderResponse
-
-        const saveOrderInDb = await prisma.order.create({
-            data:{
-                amount:fetchPricingPlan.price,
-                clerkId:parsedData.data.clerkId,
-                subscription:fetchPricingPlan.subscription,
-                razorPayOrderId:createNewOrder.id,
-                pricingId:parsedData.data.pricingId
-            }
-        })
-
-
-        return NextResponse.json({
-            success:true,
-            order:createNewOrder
-        })
-
-    } catch (error) {
-        console.error("error happened while creating the order: ", error);
-        return NextResponse.json({
-            success:false,
-            message:"Internal Error happend Try Again Sorry for inconvience"
-        }, {status:500})
+    if (!parsedData.success) {
+      return NextResponse.json(
+        {
+          success: false,
+          message: `validation failed: ${parsedData.error.message}`,
+        },
+        { status: 400 }
+      );
     }
+
+    // lets check active subscription
+
+    const checkSubscription = await prisma.activeSubscription.findFirst({
+      where: {
+        clerkId: parsedData.data.clerkId,
+        isCancelled: false,
+        expiryDate: {
+          gt: new Date(),
+        },
+      },
+    });
+
+    if (checkSubscription) {
+      return NextResponse.json(
+        { success: true, message: "You already have an active subscription" },
+        { status: 200 }
+      );
+    }
+
+    const fetchPricingPlan = await prisma.pricing.findUnique({
+      where: {
+        id: parsedData.data.pricingId,
+      },
+    });
+
+    if (!fetchPricingPlan) {
+      return NextResponse.json(
+        {
+          success: false,
+          message: "could not find the plan",
+        },
+        { status: 404 }
+      );
+    }
+
+    const createNewOrder = (await razorPay.orders.create({
+      amount: fetchPricingPlan.price,
+      currency: fetchPricingPlan.currency,
+      receipt: parsedData.data.clerkId,
+      notes: {
+        Subscription: fetchPricingPlan.subscription,
+        clerkId: parsedData.data.clerkId,
+      },
+    })) as RazorpayOrderResponse;
+
+    const saveOrderInDb = await prisma.order.create({
+      data: {
+        amount: fetchPricingPlan.price,
+        clerkId: parsedData.data.clerkId,
+        subscription: fetchPricingPlan.subscription,
+        razorPayOrderId: createNewOrder.id,
+        pricingId: parsedData.data.pricingId,
+      },
+    });
+
+    return NextResponse.json({
+      success: true,
+      order: createNewOrder,
+    });
+  } catch (error) {
+    console.error("error happened while creating the order: ", error);
+    return NextResponse.json(
+      {
+        success: false,
+        message: "Internal Error happend Try Again Sorry for inconvience",
+      },
+      { status: 500 }
+    );
+  }
 }
-
-
